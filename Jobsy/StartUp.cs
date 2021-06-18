f08a46e600b685856e119fae5c462133f0e7a8c9
Alejandro M <alejandromatos1302@gmail.com>
2021-06-18 17:12:14 -0400

    Packages installed
    I installed important packages for the project

---
+++ Jobsy/StartUp.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Owin;
+using Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+[assembly: OwinStartup(typeof(Jobsy.App_Start.StartUp))]
+namespace Jobsy.App_Start
+{
+
+    public partial class StartUp
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
