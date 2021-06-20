705af7bb9116946e65093e2701f2602d9843fc67
Alejandro M <alejandromatos1302@gmail.com>
2021-06-20 14:42:00 -0400

    Authentication completed

---
+++ Jobsy_API/Controllers/AuthenticationAPIController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using DATA_L.Authentication;
+using ENTITY_L.Models.Authentication;
+
+namespace Jobsy_API.Controllers
+{
+    public class AuthenticationAPIController : ApiController
+    {
+        AuthenticationD Auth = new AuthenticationD();
+
+        [HttpPost]
+        [Route("api/Auth/Login")]
+        public Task<LoginModel> Login(LoginModel model)
+        {
+            var _model = Auth.LoginAsync(model);
+            return _model;
+        }
+
+        [HttpPost]
+        [Route("api/Auth/SignUp")]
+        public Task<SignUpModel> SignUp(SignUpModel model)
+        {
+            var _model = Auth.RegisterAsync(model);
+            return _model;
+        }
+
+        [HttpPost]
+        [Route("api/SignUpTest")]
+        public string SignUpTest(SignUpModel model)
+        {
+            return $"Su nombre es {model.Name}, su email es: {model.Email} y su contraseña: {model.Password}";
+        }
+    }
+}
