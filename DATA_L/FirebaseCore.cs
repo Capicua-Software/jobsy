f57c4fc4664374edc931521e53a653df27167b37
Alejandro M <alejandromatos1302@gmail.com>
2021-06-20 20:14:35 -0400

    Roles management implemented
    Roles management between Standard Users and Employers already done, it sends info to Firestore

--- DATA_L/FirebaseCore.cs
+++ DATA_L/FirebaseCore.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Google.Cloud.Firestore;
 
 namespace DATA_L.AuthenticationD
 {
@@ -13,5 +14,16 @@ namespace DATA_L.AuthenticationD
 
         public static string SecretKey = "1ojW1J6ZrTZ31IDYC9s65fLBVLUloQLxETpHsOBa";
         public static string BasePath = "https://jobsy-e4cf0-default-rtdb.firebaseio.com/";
+
+        //FIRESTORE
+        public static string path = AppDomain.CurrentDomain.BaseDirectory + @"jobsy-e4cf0-firebase-adminsdk-1h2h2-0d91e1653c.json";
+        public static FirestoreDb db;
+        public void OpenFirestoreConnection()
+        {
+            Environment.SetEnvironmentVariable("GOOGLE_APPLICATION_CREDENTIALS", path);
+            db = FirestoreDb.Create("jobsy-e4cf0");
+        }
+
+        public static DocumentReference docRef;
     }
 }
