7be318ea8ec8aad16171de04f3ca0baf0bfb7adb
Jannabel Ramos Ramírez <jannabelramos@gmail.com>
2021-06-20 23:58:05 -0400

    Insert jobs feature completed

---
+++ DATA_L/JobsD/JobsD.cs
@@ -0,0 +1,44 @@
+﻿using DATA_L.AuthenticationD;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ENTITY_L.Models.Jobs;
+using Google.Cloud.Firestore;
+
+namespace DATA_L.JobsD
+{
+    public class JobsD:FirebaseCore
+    {
+        public async Task<JobsModel> PostJobsAsync(JobsModel model)  // Metodo para guardar un empleo en Firestore
+        {
+            OpenFirestoreConnection(); // Establece la conexión
+            try
+            {
+             Dictionary<string, object> job = new Dictionary<string, object> //Diccionario de datos con los campos y sus respectivos valores
+            {
+                { "Compañía", model.Company },
+                { "Tipo de Empleo", model.JobType },
+                { "Logo", model.Logo },
+                { "URL", model.URL },
+                { "Posición", model.Job },
+                { "Ubicación", model.Location },
+                { "Categoría", model.Category },
+                { "Descripción del trabajo", model.JobDescription },
+                { "Cómo aplicar", model.Requirements },
+                { "Email", model.Email }
+            };
+
+                docRef = await db.Collection("Jobs").AddAsync(job); // Guardar en la colección de Jobs el diccionario
+                return model; // Retorna el modelo
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Debug.WriteLine(ex.Message); // En caso de una excepción imprime más información en la consola
+                throw;
+            }
+          
+        }
+    }
+}
--- ENTITY_L/Models/Jobs/JobsModel.cs
+++ ENTITY_L/Models/Jobs/JobsModel.cs
@@ -1,12 +1,50 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ENTITY_L.Models.Jobs
 {
-    class JobsModel
+    public class JobsModel
     {
+        [Required]
+        [DataType(DataType.Text)]
+        [Display(Name = "Company")]
+        public string Company { get; set; }
+
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Text)]
+        public string JobType { get; set; }
+
+        public string Logo { get; set; }
+        public string URL { get; set; }
+
+        [Required]
+        [DataType(DataType.Text)]
+        public string Job { get; set; }
+
+        [Required]
+        [DataType(DataType.Text)]
+        public string Location { get; set; }
+
+        [Required]
+        [DataType(DataType.Text)]
+        public string Category { get; set; }
+
+        [Required]
+        [DataType(DataType.Text)]
+        public string JobDescription { get; set; }
+
+        [Required]
+        [DataType(DataType.MultilineText)]
+        public string Requirements { get; set; }
+
     }
 }
