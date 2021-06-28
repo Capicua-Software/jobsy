735508dcdbe522f45025fe4dfecfd0ae95295b45
Jannabel Ramos Ramírez <jannabelramos@gmail.com>
2021-06-28 00:11:23 -0400

    Issues Fixed, Category CRUD

---
+++ ENTITY_L/Models/Category/CategoryModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Google.Cloud.Firestore;
+
+
+namespace ENTITY_L.Models.Category
+{
+    [FirestoreData]
+    public class CategoryModel
+    {
+        [DataType(DataType.Text)]
+        [FirestoreProperty]
+        public string Id { get; set; }
+
+        [DataType(DataType.Text)]
+        [FirestoreProperty]
+        public string Name { get; set; }
+
+    }
+}
